ecef2d1c7f6b294ebcdb70c502939d3eef90f61e
Max Bruce <maxbruce@student-10-201-05-215.wireless.adelaide.edu.au>
2022-08-15 14:06:46 +0930

    cheer

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -12,20 +12,20 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n)
             second[i][j] = first[i][j];
         }
     }
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<2; j++)
-        {
-            if(j!=1)
-            {
-              cout << second[i][j] << " ";  
-            }
-            else
-            {
-                cout << second[i][j];
-            } 
-        }
-        cout << endl;
-    }
+    // for(int i=0; i<n; i++)
+    // {
+    //     for(int j=0; j<2; j++)
+    //     {
+    //         if(j!=1)
+    //         {
+    //           cout << second[i][j] << " ";  
+    //         }
+    //         else
+    //         {
+    //             cout << second[i][j];
+    //         } 
+    //     }
+    //     cout << endl;
+    // }
     return;
 }
